ad573f6225f628593bc1a83a76f9e35cf0f506b4
mayu.hiruta <k.khg42.206@gmail.com>
2024-05-13 22:32:42 +0900

    加筆修正0513

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,19 @@
-$('.carousel').slick({
-    autoplay: false,
-    arrows: false,
-    dots: true,
-    centerMode: true,
-    variableWidth: true,
-});
+if (window.matchMedia( "(max-width: 768px)" ).matches) {
+    $('.carousel').slick({
+        autoplay: false,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        variableWidth: true,
+    }
+    );  
+} else {
+    $('.carousel').slick({
+        autoplay: false,
+        arrows: false,
+        dots: true,
+        slidetoShow: 3,
+        slidetoScroll: 1,
+        }
+    );
+}
